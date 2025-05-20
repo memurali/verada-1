061e53d3a572bdd212f257a10b795053386e2c86
Murali <murali@progressive-solution.com>
2025-05-20 12:38:27 +0530

    Update login.js

--- staticfiles/js/login.js
+++ staticfiles/js/login.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
             body: JSON.stringify({ email, password })
         })
         .then(response => response.json())
-            consolse.log(response,response.json(),".")
+            consolse.log(response,">>>>>>>>>>>.")
                 
         .then(data => {
             console.log(response,data, "response")
